6f3156dbf839437d377b4d4ef4299f30fc6d3d72
Anzor Ishak <anzori@spacelift.io>
2025-09-19 10:33:42 -0400

    large-value-repro

---
+++ spacelift-large-value-repro/main.tf
@@ -0,0 +1,10 @@
+resource "kubernetes_config_map_v1" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = file("${path.module}/maproles.yaml")
+  }
+}
---
+++ spacelift-large-value-repro/provider.tf
@@ -0,0 +1,6 @@
+provider "kubernetes" {
+  host                   = "https://example.invalid"
+  token                  = "null"
+  insecure               = true
+  config_context_cluster = "null"
+}
