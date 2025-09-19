c41a5c8d3495958a403b4a5ad665ebea4194a6e2
Anzor-Ishak <anzori@spacelift.io>
2025-09-19 11:38:46 -0400

    Delete spacelift-large-value-repro/provider.tf

--- spacelift-large-value-repro/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "kubernetes" {
-  host                   = "https://example.invalid"
-  token                  = "fake"
-  insecure               = true
-  config_context_cluster = "false"
-}
