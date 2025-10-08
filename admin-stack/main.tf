be4a0155ce0c27371b7571bbcfb7e68036e0eb5e
Anzor Ishak <anzori@spacelift.io>
2025-10-08 11:34:25 -0400

    admin-stack

---
+++ admin-stack/main.tf
@@ -0,0 +1,11 @@
+# terraform {
+#   required_providers {
+#     spacelift = {
+#       source  = "spacelift-io/spacelift"
+#       version = "~> 1.0"
+#     }
+#   }
+# }
+
+# # When running *inside* Spacelift, no explicit credentials are required.
+# provider "spacelift" {}
