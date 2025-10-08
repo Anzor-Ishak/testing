7b164f0ee707ad487d886402bc599078e1d252c5
Anzor-Ishak <anzori@spacelift.io>
2025-10-08 12:10:59 -0400

    Update variables.tf

--- admin-stack/variables.tf
+++ admin-stack/variables.tf
@@ -1,10 +1,1 @@
-variable "space_id" {
-  description = "The Space where the role will apply (e.g. child space slug)"
-  type        = string
-}
-
-variable "key_name" {
-  description = "Human-friendly name for the API key to create"
-  type        = string
-  default     = "tf-created-automation-key"
-}
+#test
