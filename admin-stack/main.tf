c9417610c3644f83ab23cc45c98c210a3ba35f05
Anzor-Ishak <anzori@spacelift.io>
2025-10-08 13:32:35 -0400

    Update main.tf

--- admin-stack/main.tf
+++ admin-stack/main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "~> 1.0"
+    }
+  }
+}
+
 provider "spacelift" {
-  alias            = "api"
   api_key_endpoint = "https://anzor-ishak.app.spacelift.io"
   api_key_id       = var.api_key_id
   api_key_secret   = var.api_key_secret
