8aca9ced890054671dbd1f696f525302dca807ce
Anzor-Ishak <anzori@spacelift.io>
2025-10-08 13:33:29 -0400

    Update main.tf

--- admin-stack/main.tf
+++ admin-stack/main.tf
@@ -16,3 +16,6 @@ provider "spacelift" {
 resource "spacelift_api_key" "test1" { 
   name        = "Test API Key 1"
 } 
+
+variable "api_key_id"     { type = string; sensitive = true }
+variable "api_key_secret" { type = string; sensitive = true }
