364cf728c4cd8d372ed905b7afbf4da8eaf0febd
Anzor-Ishak <anzori@spacelift.io>
2025-10-08 13:31:28 -0400

    Update provider.tf

--- admin-stack/provider.tf
+++ admin-stack/provider.tf
@@ -1,6 +1,1 @@
-provider "spacelift" {
-  alias            = "api"
-  api_key_endpoint = "https://anzor-ishak.app.spacelift.io"
-  api_key_id       = var.api_key_id
-  api_key_secret   = var.api_key_secret
-}
+#
