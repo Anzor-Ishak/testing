a082f9eb7df91d8aad3e8d4e6043a7bbeb8954e4
Anzor-Ishak <anzori@spacelift.io>
2025-10-08 12:10:51 -0400

    Update provider.tf

--- admin-stack/provider.tf
+++ admin-stack/provider.tf
@@ -1,16 +1,1 @@
-terraform {
-  required_providers {
-    spacelift = {
-      source  = "spacelift-io/spacelift"
-      version = "~> 1.0"
-    }
-  }
-}
-
-provider "spacelift" {
-  # When running outside Spacelift, use an API key:
-  # (set these as environment variables before 'init/plan/apply')
-  # SPACELIFT_API_KEY_ENDPOINT
-  # SPACELIFT_API_KEY_ID
-  # SPACELIFT_API_KEY_SECRET
-}
+#test
